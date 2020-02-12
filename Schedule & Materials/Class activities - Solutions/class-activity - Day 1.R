6c9481c7ecbf2f3f51e45d23d7d646b7d3fd9dfb
Emmanuel Olamijuwon <emmanuel@olamijuwon.com>
2020-02-12 19:35:56 +0100

    updt

--- Schedule & Materials/Class activities - Solutions/class-activity - Day 1.R
+++ Schedule & Materials/Class activities - Solutions/class-activity - Day 1.R
@@ -1,11 +1,14 @@
-install
+
 
 
 library (dplyr)
 library (readstata13)
 library (mosaic)
-
-agyw_dataset <- read.dta13("C:/Users/eOlamijuwon/OneDrive/Workshops_Seminars/2020/Workshops/DemogRaphic Research and Data Analysis/Data - Misc/Nigeria [DHS].dta")
+library (kableExtra)
+library(compareGroups)
+library(survey)
+                          ## Please edit the directory to reflect where the dataset is saved on your PC.
+agyw_dataset <- read.dta13("/2020/Workshops/DemogRaphic Research and Data Analysis/Data - Misc/Nigeria [DHS].dta")
 View(agyw_dataset)
 
 agyw_clean <- agyw_dataset %>% 
@@ -18,6 +21,9 @@ agyw_clean <- agyw_dataset %>%
               ## rename region and residece
               rename(region = v024) %>% 
               rename (residence = v025) %>% 
+              rename (psu = v001) %>% 
+  
+              mutate (weight = v005/10^6) %>% 
   
               mutate(mCuse = if_else((v364 != "using modern method"),
                                      "Not using Modern",
@@ -27,13 +33,103 @@ agyw_clean <- agyw_dataset %>%
                                              v106 == "primary"),
                                           "< Secondary",
                                           "Secondary +")) %>% 
-              mutate(religion = derivedFactor("Christian" = (v130 == "catholic" | v130 == "other christian"),
-                                              "Muslim" = (v130 == "islam"),
-                                              "Others" = (v130 == "traditionalist" | v130 == "other"),
-                                              .default = NA)) %>% 
+              mutate(religion = if_else((v130 == "catholic" | 
+                                           v130 == "other christian"),
+                                        "christians",
+                                        if_else((v130 == "islam"), 
+                                                "muslims", "others"))) %>% 
           select(region, mCuse, 
                  religion, teen_educa,
-                 residence)
+                 residence, weight, psu)
+
+agyw_clean$strata <- do.call(paste, agyw_clean[, c("region", "residence")])
+
+## Declare dataset in survey mode
+dhs_design <- 
+    svydesign( 
+        ~ psu , 
+        strata = ~strata , 
+        data = agyw_clean , 
+        weights = ~weight
+    )
+
+## Create a new object (data frame) with information on
+## on weighted frequency and percentage distributions
+wregion <- svytotal( ~ region , dhs_design) %>% 
+            data.frame() %>% 
+            mutate (perc = total/sum(total)) %>% 
+            mutate (perc = perc * 100) %>% 
+            mutate (perc = round(perc, digits = 2))
+
+agyw_clean$mCuse <- as.factor(agyw_clean$mCuse)       
+
+cuse_regress <- glm(mCuse ~ factor(region) + factor(residence),
+                    data = agyw_clean, family = binomial(link = "cloglog"))
+
+summary(cuse_regress)
+
+
+wregion2 <- svytable ( ~ region, dhs_design, Ntotal=TRUE)
 
 
 
+table_mCuse <- data.frame(table(agyw_clean$mCuse)) %>% 
+            mutate (perc = Freq/sum(Freq)) %>% 
+            mutate (perc = perc * 100) %>% 
+            mutate (perc = round(perc, digits = 2))
+
+table_religion <- data.frame(table(agyw_clean$religion)) %>% 
+            mutate (perc = Freq/sum(Freq)) %>% 
+            mutate (perc = perc * 100) %>% 
+            mutate (perc = round(perc, digits = 2))
+
+table_region <- data.frame(table(agyw_clean$region)) %>% 
+            mutate (perc = Freq/sum(Freq)) %>% 
+            mutate (perc = perc * 100) %>% 
+            mutate (perc = round(perc, digits = 2))
+
+table_residence <- data.frame(table(agyw_clean$residence)) %>% 
+            mutate (perc = Freq/sum(Freq)) %>% 
+            mutate (perc = perc * 100) %>% 
+            mutate (perc = round(perc, digits = 2))
+
+table_teen_educa <- data.frame(table(agyw_clean$teen_educa)) %>% 
+            mutate (perc = Freq/sum(Freq)) %>% 
+            mutate (perc = perc * 100) %>% 
+            mutate (perc = round(perc, digits = 2))
+
+
+univar_table <- rbind(table_mCuse, table_teen_educa,
+                      table_region, table_religion,
+                      table_residence) %>% 
+                rename("Characteristics" = "Var1",
+                       "Frequency" = "Freq",
+                       "Percentages (%)" = "perc")
+
+univar_table %>% 
+  kable() %>% 
+  kable_styling("striped", full_width = F) %>% 
+  pack_rows("Modern Contraceptive Use", 1, 2) %>% 
+  pack_rows("Education", 3, 4) %>% 
+  pack_rows("Region", 5, 10) %>% 
+  pack_rows("Religious Affiliation", 11, 13) %>% 
+  pack_rows("Residence", 14, 15) %>% 
+  footnote(general = "Here is a general comments of the table. ",
+           symbol = c("< Secondary: Refers to AGYW with less than secondary education ",
+                      "Secondary +: Refers to AGYW with Secondary or more education ")) %>% 
+  save_kable(file = "C:/Users/eOlamijuwon/Desktop/descriptiveTable.html", self_contained = T)
+
+
+
+table <- descrTable(agyw_clean)
+
+## Export unweighted frequency table to html or docx
+export2html(table, file = "descriptTable.html")
+export2word(table, file = "descriptTable.docx")
+
+## Create an unweighted crossTable
+cross_tab <- descrTable(mCuse ~ teen_educa + region, agyw_clean)
+cross_tab
+
+
+export2word(cross_tab, "cross_tab.docx")
